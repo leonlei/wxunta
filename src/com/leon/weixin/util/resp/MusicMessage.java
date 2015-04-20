0258ea79fdcdb1087659ef1856cbbc314762ba5c
leon <819661766@qq.com>
2015-04-20 18:51:32 +0800

    2015-04-20 18:51

---
+++ src/com/leon/weixin/util/resp/MusicMessage.java
@@ -0,0 +1,14 @@
+package com.leon.weixin.util.resp;
+
+public class MusicMessage extends BaseMessage{
+	   // “Ù¿÷  
+    private Music Music;  
+  
+    public Music getMusic() {  
+        return Music;  
+    }  
+  
+    public void setMusic(Music music) {  
+        Music = music;  
+    } 
+}
